77be56fa0e9ed60804afbf5c45f87ca9aa0e2bf5
wangly19 <18607948581@163.com>
2022-10-19 23:56:38 +0800

    feat: initial @caster-fiber/materials library

---
+++ package/materials/vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.tsx"),
+      name: "casterMaterials",
+      formats: ["es", "umd"],
+      fileName: (format) => `main.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@craftjs/core"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
