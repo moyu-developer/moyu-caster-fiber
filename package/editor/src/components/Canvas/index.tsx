c6fe7df53022d2363b6dcabf74e29e5031f82ed4
wangly19 <18607948581@163.com>
2022-10-29 23:20:10 +0800

    chore: updated Editor Canvas Layout.

--- package/editor/src/components/Canvas/index.tsx
+++ package/editor/src/components/Canvas/index.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { Frame, Element } from "@craftjs/core";
 import { Container, Text } from "@caster-fiber/materials";
+import { Toolbox } from './ToolBox'
+import { useStyles } from "./useStyles";
 
 export const Canvas = () => {
+  const styles = useStyles();
+
   return (
-    <div
-      style={{
-        height: "100%",
-        width: 375
-      }}
-    >
-      <Frame>
-        <Element is={Container} height="100%" canvas >
-          <Text text="111" />
-        </Element>
-      </Frame>
+    <div className={styles.wrap}>
+      <Toolbox/>
+      <div className={styles.canvas}>
+        <Frame>
+          <Element is={Container} height="100%" canvas>
+            <Text text="111" />
+          </Element>
+        </Frame>
+      </div>
     </div>
   );
 };
---
+++ package/editor/src/components/Canvas/useStyles.tsx
@@ -0,0 +1,26 @@
+import { css } from "@emotion/css"
+import { theme } from 'antd'
+
+const { useToken } = theme
+
+export const useStyles = () => {
+  
+  const { token } = useToken()
+
+  return ({
+    wrap: css({
+      height: "100%",
+      background: token.colorBgContainerDisabled,
+      display: "grid",
+      gridTemplateRows: '45px 1fr'
+    }),
+    canvas: css({
+      height: "100%",
+      width: '100%',
+      padding: token.paddingSM,
+    }),
+    toolbox: css({}),
+
+    action: css({})
+  })
+}
