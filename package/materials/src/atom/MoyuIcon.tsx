6654dff88a2976e97b80be473f3732bf4160b43d
wangly19 <18607948581@163.com>
2022-11-16 23:57:47 +0800

    chore(materials): add RemixIcon and InputNumberSetter Component

---
+++ package/materials/src/atom/MoyuIcon.tsx
@@ -0,0 +1,8 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+/**
+ * @projectId 3775381
+ */
+export const RemixIcon = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/c/font_3775381_ca69qnnum7j.js',
+});
---
+++ package/materials/src/setter/InputNumberSetter.tsx
@@ -0,0 +1,7 @@
+import { InputNumber } from 'antd'
+import styled from '@emotion/styled'
+import { typography, space, color, layout, grid } from 'styled-system'
+
+export const InputNumberSetter = styled(InputNumber)(
+  layout,
+)
