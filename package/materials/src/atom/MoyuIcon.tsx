ae5747167490b66acd706b18dee7afc0b5d47f6f
wangly19 <18607948581@163.com>
2022-11-17 23:42:22 +0800

    feat(materials): Create RemixIcon library of icon components

--- package/materials/src/atom/MoyuIcon.tsx
+++ package/materials/src/atom/MoyuIcon.tsx
@@ -4,5 +4,5 @@ import { createFromIconfontCN } from '@ant-design/icons';
  * @projectId 3775381
  */
 export const RemixIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_3775381_ca69qnnum7j.js',
+  scriptUrl: '//at.alicdn.com/t/c/font_3775381_1wxidagpx6c.js',
 });
