4337bb0b77668ca3003771ca11ce66abe2382441
wangly19 <18607948581@163.com>
2022-11-21 23:32:11 +0800

    chore(server): Add fastify.authenticate verification

--- app/server/src/plugins/jwt.ts
+++ app/server/src/plugins/jwt.ts
@@ -3,12 +3,21 @@ import jwt, { JWT } from '@fastify/jwt'
 
 declare module 'fastify' {
   export interface FastifyInstance {
-    jwt: JWT
+    jwt: JWT,
+    authenticate: any
   }
 }
 
 export default fp(async (fastify) => {
   fastify.register(jwt, {
     secret: 'supersecret'
+  })
+
+  fastify.decorate("authenticate", async (request: any, reply: any) => {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
   })
 });
