24c47fb5bade4dd56285b9504ee0887b5bfab346
wangly19 <18607948581@163.com>
2022-11-23 23:50:35 +0800

    feat(server): Add user/signup api

--- app/server/src/routes/user/signup.ts
+++ app/server/src/routes/user/signup.ts
@@ -1,34 +1,56 @@
-import { Static, Type } from '@sinclair/typebox'
-import { FastifyPluginAsync } from 'fastify'
-
-const requestSchema = {
-  description: '创建用户信息',
-  tags: ['user'],
-  summary: '注册用户',
-  body: Type.Object({
-    username: Type.String(),
-    password: Type.String(),
-    avatar: Type.Optional(Type.String()),
-    openId: Type.Optional(Type.String()),
-    originType:  Type.Optional(Type.Number())
-  }),
-  response: {
-    200: Type.Object({
-      id: Type.String()
-    })
-  }
-}
+import { Static, Type } from "@sinclair/typebox";
+import { FastifyPluginAsync } from "fastify";
+import { hashSync } from 'bcryptjs'
 
 const login: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  const requestSchema = {
+    description: "创建用户信息",
+    tags: ["user"],
+    summary: "注册用户",
+    body: Type.Object({
+      name: Type.String(),
+      password: Type.String(),
+      avatar: Type.Optional(Type.String()),
+      openId: Type.Optional(Type.String()),
+      originType: Type.Optional(Type.Number()),
+    }),
+    response: {
+      200: fastify.handleResponseSchema(Type.Object({
+        id: Type.String(),
+      })),
+    },
+  };
+
   fastify.post<{
-    Body:Static<typeof requestSchema.body>   
-  }>('/signup', {
-    schema: requestSchema
-  }, async function (request, reply): Promise<any> {
-    return {
-      token: '1'
+    Body: Static<typeof requestSchema.body>;
+  }>(
+    "/signup",
+    {
+      schema: requestSchema,
+    },
+    async function (request, reply): Promise<any> {
+      const { originType = 1, name } = request.body;
+      const row = await fastify.prisma.user.findUnique({
+        where: {
+          name: name,
+        },
+      });
+      if (row?.id) {
+        return reply.notFound(`用户名(${name})已经存在`);
+      }
+
+      const user = await fastify.prisma.user.create({
+        data: {
+          ...request.body,
+          password: hashSync(request.body.password, 10),
+          originType,
+        },
+      });
+      return {
+        id: user.id,
+      };
     }
-  })
-}
+  );
+};
 
 export default login;
