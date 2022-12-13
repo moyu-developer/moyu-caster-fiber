545c3827f5474ff727586bbbe302944de6a0b359
wangly19 <18607948581@163.com>
2022-12-13 23:35:32 +0800

    feat(main): 添加RemixIcon组件

---
+++ app/main/src/components/RemixIcon.tsx
@@ -0,0 +1,8 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+/**
+ * @projectId 3775381
+ */
+export const RemixIcon = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/c/font_3775381_9nxbxke8uef.js',
+});
