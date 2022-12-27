33c609ea3a42567da1a41c008e40c46c37653644
wangly19 <18607948581@163.com>
2022-12-27 23:50:20 +0800

    chore(material): 新增间隔距离Setter设置

--- package/materials/src/reference/SpaceGroup.tsx
+++ package/materials/src/reference/SpaceGroup.tsx
@@ -1,96 +1,65 @@
-import { css } from "@emotion/css";
-import { Collapse, CollapsePanelProps, Form, Input, InputNumber } from "antd";
-import { ContenteditableSetter } from "@/setter/ContenteditableSetter";
-import { NumberOutlined } from "@ant-design/icons";
+import { Collapse, CollapsePanelProps, Form, Space, InputNumber } from "antd";
 
-const classes = {
-  marginBox: css({
-    height: 200,
-    display: "flex",
-    flexDirection: "column",
-  }),
-  marginText: css({
-    width: `100%`,
-    display: `flex`,
-    justifyContent: `center`,
-  }),
-  marginCenter: css({
-    width: "100%",
-    flex: 1,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  }),
-  paddingBox: css({
-    height: `100%`,
-    width: `100%`,
-    display: "flex",
-    flexDirection: "column",
-    background: `#fafafa`,
-    padding: 12,
-    margin: `12px 0`,
-  }),
-  paddingCenter: css({
-    width: `100%`,
-    flex: 1,
-    display: `flex`,
-    alignItems: `center`,
-    justifyContent: `space-between`,
-  }),
-};
+const SpaceTextMap = [
+  {
+    match: "Top",
+    value: "上",
+  },
+  {
+    match: "Right",
+    value: "右",
+  },
+  {
+    match: "Bottom",
+    value: "下",
+  },
+  {
+    match: "Left",
+    value: "左",
+  },
+];
 
 export const SpaceGroupSettings = (props: Partial<CollapsePanelProps>) => {
   return (
     <Collapse.Panel {...props} key="SpaceGroupSetter" header="间距">
-      <div className={classes.marginBox}>
-        <div className={classes.marginText}>
-          <Form.Item noStyle name="mt">
-            <ContenteditableSetter />
-          </Form.Item>
-        </div>
-        <div className={classes.marginCenter}>
-          <Form.Item noStyle name="ml">
-            <ContenteditableSetter />
-          </Form.Item>
-          <div className={classes.paddingBox}>
-            <div className={classes.marginText}>
-              <Form.Item noStyle name="pt">
-                <ContenteditableSetter />
-              </Form.Item>
-            </div>
-            <div className={classes.paddingCenter}>
-              <Form.Item noStyle name="pl">
-                <ContenteditableSetter />
-              </Form.Item>
-              <div>
-                <NumberOutlined
-                  style={{
-                    fontSize: 24,
-                  }}
-                />
-              </div>
-              <Form.Item noStyle name="pr">
-                <ContenteditableSetter />
-              </Form.Item>
-            </div>
-            <div className={classes.marginText}>
-              <Form.Item noStyle name="pb">
-                <ContenteditableSetter />
-              </Form.Item>
-            </div>
-          </div>
-          <div>
-            <Form.Item noStyle name="mr">
-              <ContenteditableSetter />
-            </Form.Item>
-          </div>
-        </div>
-        <div className={classes.marginText}>
-          <Form.Item noStyle name="mb">
-            <ContenteditableSetter />
-          </Form.Item>
-        </div>
-      </div>
+      <Form.Item label="Margin" tooltip="外边距设置">
+        <Space.Compact block>
+          {["marginTop", "marginRight", "marginBottom", "marginLeft"].map(
+            (key) => {
+              const label = SpaceTextMap.find((v) => key.includes(v.match));
+              return (
+                <Form.Item key={key} noStyle name={key}>
+                  <InputNumber<string>
+                    controls={false}
+                    placeholder={label?.value}
+                    formatter={(value: string | undefined) => value ? `${value}px` : ''}
+                    parser={(value: string | undefined) => value!.replace("px", "")}
+                  />
+                </Form.Item>
+              );
+            }
+          )}
+        </Space.Compact>
+      </Form.Item>
+      <Form.Item label="Padding" tooltip="内边距设置">
+        <Space.Compact block>
+          {["paddingTop", "paddingRight", "paddingBottom", "paddingLeft"].map(
+            (key) => {
+              const label = SpaceTextMap.find((v) => key.includes(v.match));
+              return (
+                <Form.Item key={key} noStyle name={key}>
+                  <InputNumber<string>
+                    controls={false}
+                    placeholder={label?.value}
+                    formatter={(value: string | undefined) => value ? `${value}px` : ''}
+                    parser={(value: string | undefined) => value!.replace("px", "")}
+                  />
+                </Form.Item>
+              );
+            }
+          )}
+        </Space.Compact>
+      </Form.Item>
     </Collapse.Panel>
   );
 };
