a8e9bb7b2d0c61bf052e607516860b9af223871a
wangly19 <18607948581@163.com>
2023-01-29 23:43:22 +0800

    chore: 添加 origin schema 声明.

--- app/server/src/routes/origin/schema.ts
+++ app/server/src/routes/origin/schema.ts
@@ -1,9 +1,19 @@
 import { Type } from "@sinclair/typebox"
 
 export const OriginDBSchema = Type.Object({
-  id: Type.String(),
-  name: Type.String(),
-  type: Type.Number(),
-  createdAt: Type.Optional(Type.String()),
-  updatedAt: Type.Optional(Type.String()),
+  id: Type.String({
+    description: '渠道id'
+  }),
+  name: Type.String({
+    description: '渠道名称'
+  }),
+  type: Type.Number({
+    description: '渠道类型'
+  }),
+  createdAt: Type.Optional(Type.String({
+    description: '创建时间'
+  })),
+  updatedAt: Type.Optional(Type.String({
+    description: '更新时间'
+  })),
 })
